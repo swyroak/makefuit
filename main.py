6b43e117b8caba6661951b1592f0d2fd010ae232
swyroak <947x9i@bma.biglobe.ne.jp>
2019-05-07 21:54:57 +0900

    etetetee

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask import render_template
+from flask import request
 from fruitname import fruitname
 
 app = Flask(__name__)
@@ -6,9 +8,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
+    return render_template('main.html', name='ボタンを押すとみの名前が出るよ')
 
-    return fruitname()
+
+@app.route('/test', methods=['POST', 'GET'])
+def test():
+    name = fruitname()
+    return render_template('main.html', name=name)
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
+    # app.run('0.0.0.0',port=8080)
